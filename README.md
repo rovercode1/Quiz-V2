af64a8df5c19c8fd62e0ad4db073b817ba00c347
Hayden Worden <haydenworden@outlook.com>
2018-04-12 13:50:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#Version 2
+
+#In Version 2 changes:
+
+        #Implemented a 'gameover' - when the correct answer is clicked the user can no longer click other options.
+
+#Currently working on:
+        
+        #Have a score count.
+        #Have a timer.
+
---
+++ new.js
@@ -0,0 +1,36 @@
+
+
+var question = document.querySelector("#question");
+var options = document.querySelectorAll(".options")
+var answer = document.querySelector("#answer");
+
+var questionOne = question.innerHTML="Which is the incorrect phrase to greet someone with?"
+var optionsOne = ["Hello","Hi","Hiya","Goodbye"]
+
+var rightAnswer=null
+
+
+var gameOver = false;
+
+
+for(i=0;i<options.length;i++){
+    options[i].innerHTML=optionsOne[i];
+    options[i].addEventListener("click",function(){
+        var clicked = this.innerHTML;
+
+        rightAnswer=optionsOne[3];
+
+        // Check if answer is correct
+            if(gameOver==false){
+                if(clicked!==rightAnswer){
+            return  this.style.color="red";
+        }else if(clicked==rightAnswer)
+        // Get colour of clicked square
+            this.style.color="rgb(76, 235, 76)"; 
+            answer.innerHTML="Correct!: blah blah blah";
+            gameOver=true;
+        }
+    gameOver=true
+    
+        } 
+    )}
